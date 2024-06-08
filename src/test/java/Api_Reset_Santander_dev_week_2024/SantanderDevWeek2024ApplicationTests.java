c5c49edcef56055028342b5ef8a8e63c7cdf537f
Claudio Roque <claudio_roque10@hotmail.com>
2024-06-08 18:41:41 -0300

    Initial commit

---
+++ src/test/java/Api_Reset_Santander_dev_week_2024/SantanderDevWeek2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package Api_Reset_Santander_dev_week_2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevWeek2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
